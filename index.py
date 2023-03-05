62cd2892ee6709061a028e40f1df91f2cf4711cb
Mustafa CAN <mcqn14@gmail.com>
2023-03-05 17:46:32 +0300

    whisper included but not done yet

--- index.py
+++ index.py
@@ -2,6 +2,10 @@ import openai
 import os
 import streamlit as st
 from streamlit_chat import message
+import pyaudio
+import wave
+import whisper
+import numpy
 
 # define a history if there is none
 if "history" not in st.session_state:
@@ -50,6 +54,54 @@ def print_history():
             message(i["content"], is_user=True)
     st.session_state.input = ""
 
+def whisper_api(wf):
+    model = whisper.load_model("base")
+    result = model.transcribe(wf)
+    
+
+def record_audio():
+    chunk = 1024
+    sample_format = pyaudio.paInt16
+    channels = 2
+    fs = 44100
+    seconds = 3
+    filename = "output.wav"
+
+    p = pyaudio.PyAudio()
+
+    print('Recording')
+
+    stream = p.open(
+        format=sample_format,
+        channels=channels,
+        rate=fs,
+        frames_per_buffer=chunk,
+        input=True)
+    
+    frames = []
+
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(numpy.fromstring(data, dtype=numpy.int16))
+
+    numpydata = numpy.hstack(frames)
+    
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    print('Finished recording')
+
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    whisper_api(filename)
+    wf.close()
+    
+
 
 st.text_input("Type something", key="input", on_change=execute_openai, placeholder="Hello, how are you?")
+st.button("Record", key="record", on_click=record_audio)
 st.text_input("OpenAI Key", key="openai_key", on_change=change_key, placeholder="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
